1a318b70bbb357142b855dc463e722aab868014e
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-02-10 02:54:37 +0600

    add audio

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 		<div className="App">
 			<Navbar />
 			<MusicInfo currentSong={currentSong} />
-			<Player />
+			<Player currentSong={currentSong} />
 		</div>
 	);
 }
--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,4 +1,12 @@
-export default function Player() {
+import { useRef } from 'react';
+
+export default function Player({ currentSong }) {
+	const songsRef = useRef();
+
+	const handlePlaySongs = () => {
+		songsRef.current.play();
+	};
+
 	return (
 		<div className="musicPlayer">
 			<div className="timeControl">
@@ -9,11 +17,15 @@ export default function Player() {
 			<div className="playControl">
 				<span className="material-icons-outlined">navigate_before</span>
 
-				<span className="material-icons-outlined">play_circle</span>
+				<span className="material-icons-outlined" onClick={handlePlaySongs}>
+					play_circle
+				</span>
 				{/* <span className="material-icons-outlined"> pause_circle</span> */}
 
 				<span className="material-icons-outlined">navigate_next</span>
 			</div>
+
+			<audio src={currentSong.audio} ref={songsRef} />
 		</div>
 	);
 }
